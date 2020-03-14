a83bf003f3abd68a8c22d47792f4fe65fb3490c7
Pablo Muñoz <pablerass@gmail.com>
2020-03-14 23:47:26 +0100

    Use react-boostrap and implement crappy login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { Button, Navbar, NavDropdown } from 'react-bootstrap';
+
 import './App.css';
+
+function LoginButton() {
+  return (
+    <Button onClick={() => window.location.href = 'http://localhost:5000/login'}>Login</Button>
+  )
+}
+
+class User extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoaded: false,
+      isLogedIn: false,
+      user: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/me", { credentials: 'include' })
+      .then((response) => {
+        if (response.status === 200) {
+          response.json().then((result) => {
+            this.setState({
+              isLoaded: true,
+              isLoggedIn: true,
+              user: result.id
+            });
+          })
+        } else if (response.status === 401) {
+          this.setState({
+            isLoaded: true,
+            isLoggedIn: false
+          });
+        } else {
+          // TODO: Raise error and break things
+        }
+      }
+    )
+  }
+
+  render() {
+    const { isLoaded, isLoggedIn, user } = this.state;
+    if (!isLoaded) {
+      return <p>Loading...</p>
+    } else if (!isLoggedIn) {
+      return <LoginButton />
+    } else {
+      return (
+        <NavDropdown title={user} id="basic-nav-dropdown">
+          <NavDropdown.Item>Log Out</NavDropdown.Item>
+        </NavDropdown>
+      )
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar bg="primary">
+      <Navbar.Brand href="#home">ipodify</Navbar.Brand>
+      <User />
+    </Navbar>
   );
 }
 

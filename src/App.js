cce14d19b5a2ee7dc282c88f9144bc2a8c10e1be
Pablo Muñoz <pablerass@gmail.com>
2020-03-15 15:17:32 +0100

    Add get playlist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Button, Navbar, NavDropdown } from 'react-bootstrap';
+import { Button, Container, Navbar, NavDropdown, Table } from 'react-bootstrap';
 
 import './App.css';
 
@@ -8,6 +8,63 @@ function LoginButton() {
   return (
     <Button onClick={() => window.location.href = 'http://localhost:5000/login'}>Login</Button>
   )
+}
+
+class Playlist extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoaded: false,
+      songs: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/library", { credentials: 'include' })
+      .then((response) => {
+        if (response.status === 200) {
+          // TODO: Find a better way to manage errors
+          response.json().then((result) => {
+            this.setState({
+              isLoaded: true,
+              songs: result.songs
+            });
+          })
+        } else {
+          // TODO: Raise error and break things
+        }
+      })
+  }
+
+  render() {
+    const { isLoaded, songs } = this.state;
+    if (!isLoaded) {
+      return <p>Loading...</p>
+    } else {
+      return (
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Artist</th>
+              <th>Album</th>
+            </tr>
+          </thead>
+          <tbody>
+            {songs.map((song) => {
+              return (
+                <tr key={song.spotify_uri}>
+                  <td>{song.name}</td>
+                  <td>{song.artists.join(", ")}</td>
+                  <td>{song.album}</td>
+                </tr>
+              )
+           })}
+          </tbody>
+        </Table>
+      )
+    }
+  }
 }
 
 class User extends Component {
@@ -71,13 +128,16 @@ class User extends Component {
 
 function App() {
   return (
-    <Navbar>
-      <Navbar.Brand>ipodify</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <User />
-      </Navbar.Collapse>
-    </Navbar>
+    <Container>
+      <Navbar>
+        <Navbar.Brand>ipodify</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <User />
+        </Navbar.Collapse>
+      </Navbar>
+      <Playlist />
+    </Container>
   );
 }
 
